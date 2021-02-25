e26fd804c18454694a4fdf9dfc613436ba4d5452
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2021-02-25 01:58:52 -0300

    create a FrameRow abstraction, and Display it using a single write!

--- src/game/frame_buffer.rs
+++ src/game/frame_buffer.rs
@@ -1,34 +1,42 @@
 use crate::colors::Style;
-use std::fmt::{self, Formatter};
+use itertools::Itertools;
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Clone, Debug)]
-pub struct FrameBuffer(Vec<Vec<Option<(Style, char)>>>);
+pub struct FrameBuffer(Vec<FrameRow>);
+
+#[derive(Clone, Debug)]
+pub struct FrameRow(Vec<Option<(Style, char)>>);
 
 impl FrameBuffer {
     pub(super) fn new(size: (usize, usize)) -> FrameBuffer {
-        FrameBuffer((0..size.1).map(|_| vec![None; size.0]).collect())
+        FrameBuffer((0..size.1).map(|_| FrameRow(vec![None; size.0])).collect())
     }
 
     pub(super) fn clear(&mut self) {
         for r in &mut self.0 {
-            for c in r {
+            for c in &mut r.0 {
                 *c = None;
             }
         }
     }
 
     pub fn draw(&mut self, pos: (usize, usize), style: Style, repr: char) {
-        *&mut self.0[pos.1][pos.0] = Some((style, repr));
+        *&mut self.0[pos.1].0[pos.0] = Some((style, repr));
     }
 
-    pub(super) fn fmt_row(&self, f: &mut Formatter<'_>, row: usize) -> fmt::Result {
-        for p in &self.0[row] {
-            if let Some((s, c)) = p {
-                write!(f, "{}", style!(s, c))?;
-            } else {
-                write!(f, " ")?;
-            };
-        }
-        Ok(())
+
+impl Display for FrameRow {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.0.iter().format_with("", |p, f| {
+                match p {
+                    None => f(&" "),
+                    Some((s, c)) => f(&style!(s, c)),
+                }
+            })
+        )
     }
 }
