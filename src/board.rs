7e4bef38325d9dd4fca885f27ee9ec85bd776897
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2021-03-09 21:03:28 -0300

    why the render time skyrockets like this??

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,6 @@
+use crate::colors::Style;
 use crate::game::FrameBuffer;
-use crate::utils::Size;
+use crate::utils::{Point, Size};
 
 #[derive(Debug)]
 pub struct Board {
@@ -11,5 +12,16 @@ impl Board {
         Board { size }
     }
 
-    pub fn draw_to(&self, _frame_buffer: &mut FrameBuffer) {}
+    pub fn draw_to(&self, frame_buffer: &mut FrameBuffer) {
+        let (border_x, border_y) = (self.size.w - 1, self.size.h - 1);
+        // FIXME: WHY the render time skyrockets if I uncomment any of these lines?? The lines below seems almost identical and are fine...
+        for x in 0..self.size.w {
+            // frame_buffer.draw(Point { x, y: 0 }, Style::ORANGE, '█');
+            // frame_buffer.draw(Point { x, y: border_y }, Style::ORANGE, '█');
+        }
+        for y in 0..self.size.h {
+            frame_buffer.draw(Point { x: 0, y }, Style::ORANGE, '█');
+            frame_buffer.draw(Point { x: border_x, y }, Style::ORANGE, '█');
+        }
+    }
 }
