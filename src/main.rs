1d04790177deee8e81b30209341336db324a5d28
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2021-03-15 15:09:57 -0300

    use termion, to leverage raw mode, input keys without echoing, get screen size

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,29 @@ mod game;
 mod utils;
 
 use game::Game;
+use std::io::{self, stdin, stdout, Write};
 use std::thread;
 use std::time::{Duration, Instant};
+use termion::cursor::Goto;
+use termion::raw::IntoRawMode;
+use termion::{clear, terminal_size};
 use utils::Size;
 
 const FRAMES_PER_SECOND: u32 = 30;
 const SKIP_TICKS: i64 = 1000 / FRAMES_PER_SECOND as i64;
 fn main() -> io::Result<()> {
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(stdout, "{}", clear::All)?;
 
-    let mut game = Game::new(Size { w: 100, h: 30 }, 20);
+    let size = terminal_size().map_or(Size { w: 100, h: 30 }, |(w, h)| Size { w, h });
+    let mut game = Game::new(
+        Size {
+            w: size.w,
+            h: size.h - 1,
+        },
+        20,
+    );
+
     loop {
         let start = Instant::now();
         // main game loop.
@@ -26,18 +40,22 @@ fn main() -> io::Result<()> {
         let render_instant = Instant::now();
         let frame_time = (render_instant - start).as_millis(); // TODO how to sample the average `frame_time` per second?
 
+        write!(
+            stdout,
+            "{}balls: {}  frame_time: {:2} ({:2} {:2} {:2})",
+            Goto(1, size.h),
+            game.balls.len(),
+            frame_time,
+            (input_end - start).as_millis(),
+            (update_end - input_end).as_millis(),
+            (render_end - update_end).as_millis(),
+        )?;
+        stdout.flush()?;
+
         let sleep_time = SKIP_TICKS - frame_time as i64;
         if sleep_time >= 0 {
             thread::sleep(Duration::from_millis(sleep_time as u64));
         }
-        println!(
-            "{:2} ({:2} {:2} {:2})\x1b[K\x1b[{}A",
-            frame_time,
-            (input_instant - start).as_millis(),
-            (update_instant - input_instant).as_millis(),
-            (render_instant - update_instant).as_millis(),
-            game.board.size.h + 2
-        );
     }
     Ok(())
 }
