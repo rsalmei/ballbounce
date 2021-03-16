449e4f31630cc7186978a99a54e213f421195c2f
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2021-03-15 22:14:56 -0300

    implement component for board

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,5 @@
 use crate::colors::Style;
-use crate::game::FrameBuffer;
+use crate::game::{Component, FrameBuffer, World};
 use crate::utils::{Point, Size};
 
 #[derive(Debug)]
@@ -11,17 +11,24 @@ impl Board {
     pub fn new(size: Size) -> Board {
         Board { size }
     }
+}
 
-    pub fn draw_to(&self, frame_buffer: &mut FrameBuffer) {
+impl Component for Board {
+    fn update(&mut self, world: &World) {}
+
+    fn draw_to(&self, frame_buffer: &mut FrameBuffer) {
         let (border_x, border_y) = (self.size.w - 1, self.size.h - 1);
-        // FIXME: WHY the render time skyrockets if I uncomment any of these lines?? The lines below seems almost identical and are fine...
-        for x in 0..self.size.w {
-            // frame_buffer.draw(Point { x, y: 0 }, Style::ORANGE, '█');
-            // frame_buffer.draw(Point { x, y: border_y }, Style::ORANGE, '█');
-        }
-        for y in 0..self.size.h {
+        (0..self.size.w).for_each(|x| {
+            frame_buffer.draw(Point { x, y: 0 }, Style::ORANGE, '█');
+            frame_buffer.draw(Point { x, y: border_y }, Style::ORANGE, '█');
+        });
+        (1..self.size.h - 1).for_each(|y| {
             frame_buffer.draw(Point { x: 0, y }, Style::ORANGE, '█');
             frame_buffer.draw(Point { x: border_x, y }, Style::ORANGE, '█');
-        }
+        });
+    }
+
+    fn size_hint(&self) -> u16 {
+        self.size.w * 2 + self.size.h * 2
     }
 }
